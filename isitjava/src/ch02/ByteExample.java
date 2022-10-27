a5c417f76c2ec0a6be589076dcbd79e999ad2251
은지 <midzy0228@gmail.com>
2022-10-27 13:09:39 +0900

    [update] byte
    byte 타입 연습

---
+++ isitjava/src/ch02/ByteExample.java
@@ -0,0 +1,14 @@
+package ch02;
+
+public class ByteExample {
+    public static void main(String[] args){
+        byte var1 = -128;
+        byte var2 = -30;
+        byte var3 = 0;
+        byte var4 = 30;
+        byte var5 = 127;
+        // byte var6 = 128; 컴파일 에러(Type mismatch: cannot covert from int to byte)
+        // byte의 범위는 -128 ~ 127
+        // 2**9;
+    }
+}
