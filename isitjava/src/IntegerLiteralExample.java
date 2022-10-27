e241ced618dcd27cd5d62764c02d5b590d01a3c3
은지 <midzy0228@gmail.com>
2022-10-27 13:02:21 +0900

    [update] IntegerLiteralExample
    정수 리터럴 완료

--- isitjava/src/IntegerLiteralExample.java
+++ isitjava/src/IntegerLiteralExample.java
@@ -2,5 +2,18 @@ public class IntegerLiteralExample {
     public static void main(String[] args){
         int var1 = 0b1011; // 2진수
         int var2 = 0206; // 8진수
+        int var3 = 356; // 10진수
+        int var4 = 0xB3; // 16진수
+
+        System.out.println("var1: "+var1);
+        System.out.println("var2: "+var2);
+        System.out.println("var3: "+var3);
+        System.out.println("var4: "+var4);
+
+        //* output
+        // var1: 11
+        //var2: 134
+        //var3: 356
+        //var4: 179 */
     }
 }
