d5c67be45a1adb7dcdf08920ff1e6575db6b51a6
은지 <midzy0228@gmail.com>
2022-10-28 16:16:35 +0900

    Update PrimitiveAndStringConversionExample.java

--- isitjava/src/ch02/PrimitiveAndStringConversionExample.java
+++ isitjava/src/ch02/PrimitiveAndStringConversionExample.java
@@ -9,5 +9,13 @@ public class PrimitiveAndStringConversionExample {
         System.out.println("value1: " + value1);
         System.out.println("value2: " + value2);
         System.out.println("value3: " + value3);
+
+        String str1 = String.valueOf(10);
+        String str2 = String.valueOf(3.14);
+        String str3 = String.valueOf(true);
+
+        System.out.println("str1: " + str1);
+        System.out.println("str2: " + str2);
+        System.out.println("str3: " + str3);
     }
 }
