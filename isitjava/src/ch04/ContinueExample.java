db8422dd46bea4d7c8be3716d1c90780d89c4d4b
은지 <midzy0228@gmail.com>
2022-10-30 16:53:04 +0900

    feat: continue 문

---
+++ isitjava/src/ch04/ContinueExample.java
@@ -0,0 +1,14 @@
+package ch04;
+
+public class ContinueExample {
+    public static void main(String[] args) {
+        for(int i=1; i<=10; i++) {
+            if(i%2 != 0) {
+                continue;
+            }
+            System.out.println(i + " ");
+        }
+        //output
+        //2 4 6 8 10
+    }
+}
