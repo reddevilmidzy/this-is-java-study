190ae0fa3bdc331977291931c79802bcbcadfe86
은지 <midzy0228@gmail.com>
2022-11-03 12:24:38 +0900

    Update CarExample.java

--- isitjava/src/ch06/sec06/exam1/CarExample.java
+++ isitjava/src/ch06/sec06/exam1/CarExample.java
@@ -6,21 +6,8 @@ public class CarExample {
         Car myCar = new Car();
 
         //Car 객체의 필드값 읽기
-        System.out.println("제작회사: " + myCar.company);
         System.out.println("모델명: " + myCar.model);
-        System.out.println("색깔: " + myCar.color);
-        System.out.println("최고속도: " + myCar.maxSpeed);
+        System.out.println("시동여부: " + myCar.start);
         System.out.println("현재속도: " + myCar.speed);
-
-        //Car 객체의 필드값 변경
-        myCar.speed = 60;
-        System.out.println("수정된 속도: " + myCar.speed);
     }
 }
-//output
-//제작회사: 기아자동차
-//모델명: K5
-//색깔: 검정
-//최고속도: 350
-//현재속도: 0
-//수정된 속도: 60
