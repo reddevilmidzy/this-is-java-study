af4e4ac81f91fb8350fa98e47c425d6275718263
은지 <midzy0228@gmail.com>
2022-11-03 12:24:34 +0900

    Update Car.java

--- isitjava/src/ch06/sec06/exam1/Car.java
+++ isitjava/src/ch06/sec06/exam1/Car.java
@@ -2,9 +2,7 @@ package ch06.sec06.exam1;
 
 public class Car {
     //필드 선언
-    String company = "기아자동차";
-    String model = "K5";
-    String color = "검정";
-    int maxSpeed = 350;
+    String model;
+    boolean start;
     int speed;
 }
