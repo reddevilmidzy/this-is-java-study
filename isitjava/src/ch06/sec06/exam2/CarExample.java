f4c76ef4415476b4e625e9a003ad116e0616a47e
은지 <midzy0228@gmail.com>
2022-11-03 12:24:22 +0900

    Create CarExample.java

---
+++ isitjava/src/ch06/sec06/exam2/CarExample.java
@@ -0,0 +1,26 @@
+package ch06.sec06.exam2;
+
+public class CarExample {
+    public static void main(String[] args) {
+        //Car 객체 생성
+        Car myCar = new Car();
+
+        //Car 객체의 필드값 읽기
+        System.out.println("제작회사: " + myCar.company);
+        System.out.println("모델명: " + myCar.model);
+        System.out.println("색깔: " + myCar.color);
+        System.out.println("최고속도: " + myCar.maxSpeed);
+        System.out.println("현재속도: " + myCar.speed);
+
+        //Car 객체의 필드값 변경
+        myCar.speed = 60;
+        System.out.println("수정된 속도: " + myCar.speed);
+    }
+}
+//output
+//제작회사: 기아자동차
+//모델명: K5
+//색깔: 검정
+//최고속도: 350
+//현재속도: 0
+//수정된 속도: 60
