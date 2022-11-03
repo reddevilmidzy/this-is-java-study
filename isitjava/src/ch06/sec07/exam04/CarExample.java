9c51df277d230da3bd03f5fecffbcf9be4259d88
은지 <midzy0228@gmail.com>
2022-11-03 12:44:14 +0900

    생성자 오버로딩

---
+++ isitjava/src/ch06/sec07/exam04/CarExample.java
@@ -0,0 +1,13 @@
+package ch06.sec07.exam04;
+
+public class CarExample {
+    public static void main(String[] args) {
+        Car car1 = new Car();
+
+        Car car2 = new Car("자가용");
+
+        Car car3 = new Car("자가용", "빨강");
+
+        Car car4 = new Car("택시", "검정", 200);
+    }
+}
