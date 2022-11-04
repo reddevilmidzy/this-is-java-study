e930e7984695f288f49f9e16cb41059a7746c2a2
은지 <midzy0228@gmail.com>
2022-11-04 20:38:47 +0900

    feat: final 필드와 상수

---
+++ isitjava/src/ch06/sec11/exam01/Korean.java
@@ -0,0 +1,15 @@
+package ch06.sec11.exam01;
+
+public class Korean {
+    //인스턴스 final 필드 선언
+    final String nation = "대한민국";
+    final String ssn;
+
+    //인스턴스 필드 선언
+    String name;
+
+    public Korean(String ssn, String name) {
+        this.ssn = ssn;
+        this.name = name;
+    }
+}
