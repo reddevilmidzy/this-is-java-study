605c0250301a5344d59ee5d0c4eadc434da0abf3
은지 <midzy0228@gmail.com>
2022-11-22 14:08:38 +0900

    feat: 인터페이스 상수 필드

---
+++ isitjava/src/ch08/sec03/RemoteControl.java
@@ -0,0 +1,7 @@
+package ch08.sec03;
+
+public interface RemoteControl {
+    //인터페이스에 선언된 필드는 모두 public static final 특성을 갖는다. 자동적으로 컴파일 과정에서 붙음.
+    int MAX_VOLUME = 10;
+    int MIN_VOLUME = 0;
+}
