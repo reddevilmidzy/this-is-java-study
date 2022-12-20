8ba0f5ccdfa33e98a5334fd899b7721cd9c7489b
은지 <midzy0228@gmail.com>
2022-12-20 23:27:26 +0900

    feat: set, map 연습

--- isitjava/src/ch15/sec04/exam01/HashMapExample.java
+++ isitjava/src/ch15/sec04/exam01/HashMapExample.java
@@ -1,2 +1,53 @@
-package ch15.sec04.exam01;public class HashMapExample {
+package ch15.sec04.exam01;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+public class HashMapExample {
+    public static void main(String[] args) {
+        //Map 컬렉션 생성
+        Map<String, Integer> map = new HashMap<>();
+
+        //객체 저장
+        map.put("신용권", 85);
+        map.put("홍길동", 90);
+        map.put("동장군", 95);
+        map.put("홍길동", 10); // key 가 같기 때문에 last 값 저장
+        System.out.println("총 Entry 수: " + map.size());
+        System.out.println();
+
+        //키로 값 얻기
+        String key = "홍길동";
+        int value = map.get(key);
+        System.out.println(key + ": " + map.get("홍길동"));
+        System.out.println();
+
+        //키 Set 컬렉션을 얻고, 반복해서 키와 값을 얻기
+        Set<String> keySet = map.keySet();
+        Iterator<String> keyIterator = keySet.iterator();
+        while (keyIterator.hasNext()) {
+            String k = keyIterator.next();
+            Integer v = map.get(key);
+            System.out.println(k + " : " + v);
+        }
+        System.out.println();
+
+        //엔트리 Set 컬렉션을 얻고, 반복해서 키와 값을 얻기
+        Set<Map.Entry<String, Integer>> entrySet = map.entrySet();
+        Iterator<Map.Entry<String, Integer>> entryIterator = entrySet.iterator();
+        while (entryIterator.hasNext()) {
+            Map.Entry<String, Integer> entry = entryIterator.next();
+            String k = entry.getKey();
+            Integer v = entry.getValue();
+            System.out.println(k + " : " + v);
+        }
+        System.out.println();
+
+        //키로 엔트리 삭제
+        map.remove("홍길동");
+        System.out.println("엔트리 수: " + map.size());
+        System.out.println();
+    }
 }
