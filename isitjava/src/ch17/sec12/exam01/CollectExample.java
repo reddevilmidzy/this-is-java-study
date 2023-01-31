818d5c56278a8a53bbe95679d38ffee63d99e83a
은지 <midzy0228@gmail.com>
2023-01-31 21:04:43 +0900

    feat: 필터링한 요소 수집 구현

---
+++ isitjava/src/ch17/sec12/exam01/CollectExample.java
@@ -0,0 +1,39 @@
+package ch17.sec12.exam01;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class CollectExample {
+    public static void main(String[] args) {
+        List<Student> totalList = new ArrayList<>();
+        totalList.add(new Student("홍길동", "남", 92));
+        totalList.add(new Student("김수영", "여", 87));
+        totalList.add(new Student("감자바", "남", 95));
+        totalList.add(new Student("오해영", "여", 93));
+
+        // 남학생만 묶어 List 생성
+        List<Student> maleList = totalList.stream()
+                .filter(s -> s.getSex().equals("남"))
+                .collect(Collectors.toList());
+
+
+        maleList.stream()
+                .forEach(s -> System.out.println(s.getName()));
+
+
+        System.out.println();
+
+        //학생 이름을 키, Student 객체를 값으로 갖는 Map 생성
+        Map<String, Integer> map = totalList.stream()
+                .collect(
+                        Collectors.toMap(
+                                Student::getName,
+                                Student::getScore
+                        )
+                );
+
+        System.out.println(map);
+    }
+}
